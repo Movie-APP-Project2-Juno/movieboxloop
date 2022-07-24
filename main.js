7d9e335a2752129fd42805676c23ff526e051d69
TenzinJungney <jungney5@gmail.com>
2022-07-24 10:57:56 -0400

    media query and general update

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-
 //name space object
 const moviesApp ={};
-
+moviesApp.key= '71bddb9affbe35fa416aaaadad7cac9e';
 // get input from search bar
 
 moviesApp.getInput = function(){
   const input =document.querySelector('#movieName');
-  //const subButton = document.querySelector('#submitBtn');
   const form = document.querySelector('.searchForm');
   
   form.addEventListener('submit', function(e){
    e.preventDefault();
-   //moviesApp.removeMovies();
    // remove movies
    document.querySelector('#displayResults').innerHTML="";
-   fetch(`https://api.themoviedb.org/3/search/movie?api_key=71bddb9affbe35fa416aaaadad7cac9e&language=en-US&query=${input.value}&page=1&include_adult=false`)
+   
+   // get movie data from API and display movies
+   const url = new URL('https://api.themoviedb.org/3/search/movie?')
+   url.search = new URLSearchParams({
+    api_key:moviesApp.key,
+    language:'en-US',
+    query:`${input.value}`,
+    page:'1',
+    include_adult:'false'
+   });
+   fetch(url)
      .then(function(response){
       if(response.ok){
         return response.json();
@@ -24,7 +31,6 @@ moviesApp.getInput = function(){
       
      })
      .then(function(responseData){
-        console.log(responseData);
       moviesApp.displayMovies(responseData);
      })
      .catch(function(err){
@@ -41,77 +47,65 @@ moviesApp.getInput = function(){
 
 //display results 
 
-moviesApp.displayMovies = async function(movies){
- console.log(movies.results);
+moviesApp.displayMovies = function(movies){
+ 
+  //get a list of movies and store them in a variable
  const movieLists= (movies.results).slice(0,8);
- console.log(movieLists);
-
+ 
  movieLists.forEach(function(movie){
-  // poster_img link: https://image.tmdb.org/t/p/w300/p1F51Lvj3sMopG948F5HsBbl43C.jpg
   
   //get movie's id and store it in ID variable. 
   
   const ID = movie.id;
-//   const movieOverview = movie.overview;
 
-  moviesApp.getMoviePage(ID).then(homepage =>{
-    console.log("response is? ",homepage );
-    //console.log("Homepage is ", homepage);
+  moviesApp.getMoviePage(ID).then(homepage=>{
+    // create li item
     const listItem = document.createElement('li');
+    // if the homepage link doesn't exist, trun off the link; 
+    const movieurl= (homepage!=""? 
+    `<a href= "${homepage}" target="_blank">Find Me</a>`:
+    `<a href= "javascript:void(0)">No Page</a>`)
+
+    // if the poster img doesn't exist, load dummy img. 
+
+    const moviePoster =( movie.poster_path!=null?
+    `<img src="https://image.tmdb.org/t/p/w300/${movie.poster_path}" alt="This is the movie's poster">`:
+    `<img src="./assets/dummy.png" alt="This is a placeholder image">`
+    );
+    
+    // add innerhtml to li item
     listItem.innerHTML = `
-    <img src="https://image.tmdb.org/t/p/w300/${movie.poster_path}" alt="This is the movie's poster">
+    
+    ${moviePoster}
     <div>
+      <p>Title: ${movie.title}</p>
       <p class='movieInfo'> Release Date: ${movie.release_date}</p> 
       <p> Score: ${movie.vote_average} </p> 
-      <a href= "${homepage}" target="_blank">Find More</a>
+      ${movieurl}
     </div>
 
     <div class="synopsisOverlay">
-        <p class="synopsisText">${movie.overview}</p>
+      <p class="synopsisText">${movie.overview}</p>
     </div>
      `
+     // append list item to ul 
     const ul = document.querySelector('#displayResults');
     ul.append(listItem);
   })
- 
-
   
  })
 }
 
-// // //remove display results
-// // moviesApp.removeMovies= function(){
-// //   const ul = document.querySelector('#displayResults');
-// //   ul.innerHTML="";
-// }
+
 
 //getMoviePage function
-moviesApp.getMoviePage = async function(ID){
+moviesApp.getMoviePage =  function(ID){
  // get another url to fetch more info data about the movie
- const url = `https://api.themoviedb.org/3/movie/${ID}?api_key=71bddb9affbe35fa416aaaadad7cac9e`;
-
-/*  let movieIdObject = await fetch(url)
- .then(function(response) {
-    if(response.ok) {
-        return response.json();
-    } else {
-        throw new Error(response.statusText);
-    }
+ const movieurl = new URL(`https://api.themoviedb.org/3/movie/${ID}`)
+ movieurl.search = new URLSearchParams({
+  api_key:'71bddb9affbe35fa416aaaadad7cac9e'
  })
- .then(function(responseData) {
-    return responseData;
- })
- .catch(function(err) {
-    if (err.message === "Not Found") {
-       alert("We couldn't find that movie!");
-     } else {
-       alert("Something went wrong...");
-     }
-    })
- 
- return movieIdObject; */
-
- let homepage = await fetch(url)
+ let homepage = fetch(movieurl)
     .then(function(response){
      if(response.ok){
        return response.json();
@@ -121,8 +115,7 @@ moviesApp.getMoviePage = async function(ID){
      
     })
     .then(function(responseData){
-     console.log(responseData);
-     console.log(responseData.overview);
+     
      return responseData.homepage;
     })
     .catch(function(err){
@@ -132,45 +125,12 @@ moviesApp.getMoviePage = async function(ID){
        alert("Something went wrong...");
      }
     })
-    console.log("inside homepage is ", homepage)
+    
     return homepage;
  }
 
-//  PSEUDOCODE FOR WATCHLIST
-//  make a button to click on and trigger the event handler
-// it will store the li item in
 
-const buttons = document.querySelectorAll('.tabButton');
-const tabs = document.querySelector('.tabBox');
-const tabContentEl = document.querySelectorAll('.tabContent');
 
-tabs.addEventListener('click', function(e) {
-    // console.log(e.target.dataset.id);
-    const id = e.target.dataset.id;
-    console.log(id);
-
-    /* if(id) {
-        // remove active from other buttons
-        buttons.forEach(function(btn) {
-            btn.classList.remove('active');
-            e.target.classList.add('active');
-        });
-        // hide other tab content
-        tabContentEl.forEach(function(tabContent) {
-            tabContent.classList.remove('active');
-        });
-        const element = document.getElementById(id);
-        element.classList.add('active');
-    } */
-});
-
-/* fetch('https://api.themoviedb.org/3/movie/343611?api_key=71bddb9affbe35fa416aaaadad7cac9e')
-.then(function(resp) {
-    return resp.json();
-})
-.then(function(respJson) {
-    console.log(respJson);
-}) */
 
 //make init function 
 moviesApp.init=function(){
